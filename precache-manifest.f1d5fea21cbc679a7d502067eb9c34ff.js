6d5dbd0facef2be6cde56d5471a326e136947493
Oluwaseun-Gbenle <62512861+Oluwaseun-Gbenle@users.noreply.github.com>
2020-07-10 23:41:33 +0100

    Updates

---
+++ precache-manifest.f1d5fea21cbc679a7d502067eb9c34ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0068adc7be68c13f0f4950b417694c27",
+    "url": "/Drum-machine/index.html"
+  },
+  {
+    "revision": "340271aa531c592066e5",
+    "url": "/Drum-machine/static/css/main.ee57baf1.chunk.css"
+  },
+  {
+    "revision": "0cd9f3258285c15cba0b",
+    "url": "/Drum-machine/static/js/2.ee155c99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Drum-machine/static/js/2.ee155c99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "340271aa531c592066e5",
+    "url": "/Drum-machine/static/js/main.057e1938.chunk.js"
+  },
+  {
+    "revision": "e80790fa3c434aa25bfc",
+    "url": "/Drum-machine/static/js/runtime-main.95518db7.js"
+  }
+]);
